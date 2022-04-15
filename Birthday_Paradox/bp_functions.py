f4e9bf831b86fa041e9f036f76c9959ebd65220c
yuenling1995 <45129170+yuenling1995@users.noreply.github.com>
2022-04-15 13:20:15 -0400

    Update bp_functions.py

--- Birthday_Paradox/bp_functions.py
+++ Birthday_Paradox/bp_functions.py
@@ -1,19 +1,10 @@
-import datetime, random
+import datetime
+from random import randint
 
-
-
-def get_days(num):
-	"""generate a list of birthdays based on the input number."""
-	days = []	
-
-	for i in range(num):
-		random_day = random.randint(0, 364)
-		days.append(random_day)
-	return days
 
 
 def get_birthdays(num):
-	days = get_days(num)
+	days = [randint(0, 365) for x in range(num)]
 
 	birthdays = []
 	start_date = datetime.date(2001, 1, 1)
@@ -23,8 +14,6 @@ def get_birthdays(num):
 		#print(new_date)
 		new_date_month = new_date.strftime("%b")
 		new_date_day = new_date.day 
-		#print(new_date_month)
-		#print(new_date_day)
 
 		new_birthday = new_date_month + " " + str(new_date_day)
 		birthdays.append(new_birthday)
@@ -37,35 +26,37 @@ def find_match_bday(birthdays):
 	duplicates = set()
 
 	if len(birthdays) == len(unique_birthdays):
-		return [0]
+		return None
 	else:
 		for birthday in birthdays:
 			if birthdays.count(birthday) > 1:
 				duplicates.add(birthday)
 
 		duplicates = list(duplicates)
-		return duplicates
+	return duplicates
+
 
 def run_simulations(num):
 	find_match = 0 
+
 	for i in range(100000):
 		if i % 10000 == 0:
 			print(str(i) + " simulations run...")
 
 		birthdays = get_birthdays(num)
-
 		duplicates = find_match_bday(birthdays)
 
-		if len(duplicates) >= 1:
-			find_match += 1
+		if duplicates != None:
+			if len(duplicates) >= 1:
+				find_match += 1
 
-	print("100,000 simulations run...")
-	return find_match
-
-
-def find_probability(find_match):
+	print("10,000 simulations run...")
 	probability = (find_match / 100000) * 100
-	return probability
+
+	return find_match, probability
+
+
+
 
 
 
